5fe43c523d2fb67e14870adb27c9209ac3b1fbf5
Laura Segouras <lausegouras@gmail.com>
2023-10-25 18:17:07 -0300

    login page - formik

--- frontend/src/components/schema.js
+++ frontend/src/components/schema.js
@@ -4,11 +4,11 @@ export const schema = yup.object().shape({
   login: yup
     .string()
     .email("Invalid email!")
-    .required("Error: It is necessary to fill in this field!")
+    .required("Error: It is necessary to fill the email!")
     .lowercase(),
   password: yup
     .string()
-    .required("Error: It is necessary to fill in this field!"),
+    .required("Error: It is necessary to fill the password!"),
   // confirmPassword: yup
   //   .string()
   //   .oneOf([yup.ref("password"), null], "The passwords must match!")
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,38 +1,48 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { schema } from "./../components/schema";
+import { useFormik } from "formik";
+import { useNavigate } from "react-router-dom";
+import MyContext from "./../context/MyContext";
 
 const Login = () => {
-  const [login, setLogin] = useState("");
-  const [password, setPassword] = useState("");
-  const [message, setMessage] = useState("");
+  // const [login, setLogin] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [message, setMessage] = useState("");
   const [data, setData] = useState({});
 
-  const handleLogin = (e) => {
-    setLogin(e.target.value);
-  };
+  const context = useContext(MyContext);
 
-  const handlePassword = (e) => {
-    setPassword(e.target.value);
-  };
+  const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const user = await schema.validate();
-    console.log(user);
+  // const handleLogin = (e) => {
+  //   setLogin(e.target.value);
+  // };
+
+  // const handlePassword = (e) => {
+  //   setPassword(e.target.value);
+  // };
+
+  const handleSubmit = async () => {
+    // const user = await schema.validate();
+    // console.log(user);
     const response = await fetch("http://localhost:3333/login", {
       headers: {
         "Content-Type": "application/json",
       },
       method: "POST",
       body: JSON.stringify({
-        login,
-        password,
+        // login,
+        // password,
+        login: formik.values.login,
+        password: formik.values.password,
       }),
     });
     const data = await response.json();
     console.log(data);
     setData(data);
+    context.setFullName(data.fullName);
+    navigate("/home");
   };
 
   // try {
@@ -52,27 +62,51 @@ const Login = () => {
   //   return false;
   // }
 
+  const formik = useFormik({
+    initialValues: {
+      login: "",
+      password: "",
+    },
+    validationSchema: schema,
+    onSubmit: handleSubmit,
+  });
+
+  console.log(formik.values);
   return (
     <div>
       <h1>Login</h1>
-      <form>
+      <form onSubmit={formik.handleSubmit}>
         <label>
           Login (e-mail)
-          <input type="text" onChange={handleLogin} value={data.login} />
+          <input
+            type="text"
+            name="login"
+            onChange={formik.handleChange}
+            value={formik.values.login}
+            // helperText={formik.errors.login ? <p>{formik.errors.login}</p> : ""}
+          />
+          {formik.errors.login ? (
+            <p style={{ color: "red" }}>{formik.errors.login}</p>
+          ) : (
+            ""
+          )}
         </label>
         <label>
           Password
           <input
             type="password"
-            onChange={handlePassword}
-            value={data.password}
+            name="password"
+            onChange={formik.handleChange}
+            value={formik.values.password}
           />
+          {formik.errors.password ? (
+            <p style={{ color: "red" }}>{formik.errors.password}</p>
+          ) : (
+            ""
+          )}
         </label>
 
-        <button type="submit" onClick={handleSubmit}>
-          Log In
-        </button>
-        {message ? <p>{message}</p> : ""}
+        <button type="submit">Log In</button>
       </form>
       <p>
         Not registered yet? Click <Link to="/registration">here!</Link>
