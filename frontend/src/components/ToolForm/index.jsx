6d22724daa68d182aa06aecb686698372f650c84
Laura Segouras <lausegouras@gmail.com>
2023-12-21 15:58:11 -0300

    create tools modal

--- frontend/src/components/ToolForm/index.jsx
+++ frontend/src/components/ToolForm/index.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as Style from "./styles"
 
 function ToolForm() {
+
+  const [name, setName] = useState("")
+  const [description, setDescription] = useState("")
+
+  function handleName(e) {
+    setName(e.target.value)
+  }
+
+  function handleDescription(e) {
+    setDescription(e.target.value)
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+    const data = {
+      name,
+      description,
+      available:true
+    }
+    const response = await fetch("http://localhost:3333/tools", {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data), 
+    });
+    console.log(response);
+  }
+
   return (
-    <Style.Container>Olá</Style.Container>
+    <Style.Container>
+      <Style.Label >
+        <Style.LabelTitle>Name: </Style.LabelTitle>
+        <Style.Input onChange={handleName} value={name} />
+      </Style.Label>
+      <Style.Label >
+        <Style.LabelTitle>Description: </Style.LabelTitle>
+        <Style.Input onChange={handleDescription} value={description}/>
+      </Style.Label>
+      <Style.Button type="Submit" onClick={handleSubmit}>Create Tool</Style.Button>
+    </Style.Container>
   )
 }
 
--- frontend/src/components/ToolForm/styles.js
+++ frontend/src/components/ToolForm/styles.js
@@ -1,5 +1,12 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-  
+export const Container = styled.form`  
+`
+export const Label = styled.label`
+`
+export const LabelTitle = styled.span`
+`
+export const Input = styled.input`
+`
+export const Button = styled.button`
 `
